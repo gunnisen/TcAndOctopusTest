a01e9940bb79e28e892f05e1cf1b509e6c57f795
Gunnar Sylthe <gunnar.sylthe@tv2.no>
2016-11-08 12:28:52 +0100

    Still trying...

--- TCAndOctopusTest.DbInit/Deploy.ps1
+++ TCAndOctopusTest.DbInit/Deploy.ps1
@@ -1,16 +1,9 @@
-﻿Write-Host "Will run TCAndOctopusTest.DbInit.exe..."
-& .\TCAndOctopusTest.DbInit.exe
-Write-Host "TCAndOctopusTest.DbInit.exe completed."
+﻿$contentPath = (Join-Path $OctopusOriginalPackageDirectoryPath "content")
+$fullPath = (Join-Path $contentPath "TCAndOctopusTest.DbInit.exe")
+Write-Host "Exe Path: " $fullPath
 
+cd $contentPath
+Write-Host "Working Dir: " $(Get-Location)
 
-#Write-Host "Connection String: <"$ConnectionString">"
-
-#$contentPath = (Join-Path $OctopusOriginalPackageDirectoryPath "content")
-#$fullPath = (Join-Path $contentPath "migrate.exe")
-#Write-Host "Migrate Path: " $fullPath
-
-#cd $contentPath
-#Write-Host "Working Dir: " $(Get-Location)
-
-#& ".\TCAndOctopusTest.DbInit.exe" | Write-Host
+& ".\TCAndOctopusTest.DbInit.exe" | Write-Host
 #& ".\migrate.exe" TCAndOctopusTest.Db.dll /startUpConfigrationFile=TCAndOctopusTest.Db.dll.config /connectionString=$ConnectionString /connectionProviderName="System.Data.SqlClient" /verbose | Write-Host
