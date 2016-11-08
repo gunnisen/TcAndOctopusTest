1056ce295ec00482394193b21f158e895a98f469
Gunnar Sylthe <gunnar.sylthe@tv2.no>
2016-11-08 11:59:44 +0100

    Yet another try with Deploy.ps1

--- TCAndOctopusTest.DbInit/Deploy.ps1
+++ TCAndOctopusTest.DbInit/Deploy.ps1
@@ -1,13 +1,14 @@
-﻿& ".\TCAndOctopusTest.DbInit.exe"
+﻿#& ".\TCAndOctopusTest.DbInit.exe"
 
 
-#Write-Host "Connection String: <"$ConnectionString">"
+Write-Host "Connection String: <"$ConnectionString">"
 
-#$contentPath = (Join-Path $OctopusOriginalPackageDirectoryPath "content")
-#$fullPath = (Join-Path $contentPath "migrate.exe")
-#Write-Host "Migrate Path: " $fullPath
+$contentPath = (Join-Path $OctopusOriginalPackageDirectoryPath "content")
+$fullPath = (Join-Path $contentPath "migrate.exe")
+Write-Host "Migrate Path: " $fullPath
 
-#cd $contentPath
-#Write-Host "Working Dir: " $(Get-Location)
+cd $contentPath
+Write-Host "Working Dir: " $(Get-Location)
 
+& ".\TCAndOctopusTest.DbInit.exe" | Write-Host
 #& ".\migrate.exe" TCAndOctopusTest.Db.dll /startUpConfigrationFile=TCAndOctopusTest.Db.dll.config /connectionString=$ConnectionString /connectionProviderName="System.Data.SqlClient" /verbose | Write-Host
