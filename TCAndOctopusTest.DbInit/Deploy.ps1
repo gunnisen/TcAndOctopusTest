7aa19efa182d55f365249f78fd2f533e8bc3b7ab
Gunnar Sylthe <gunnar.sylthe@tv2.no>
2016-11-08 11:15:19 +0100

    Simplified Deploy.ps1

---
+++ TCAndOctopusTest.DbInit/Deploy.ps1
@@ -0,0 +1,12 @@
+﻿& .\TCAndOctopusTest.DbInit.exe
+
+#Write-Host "Connection String: <"$ConnectionString">"
+
+#$contentPath = (Join-Path $OctopusOriginalPackageDirectoryPath "content")
+#$fullPath = (Join-Path $contentPath "migrate.exe")
+#Write-Host "Migrate Path: " $fullPath
+
+#cd $contentPath
+#Write-Host "Working Dir: " $(Get-Location)
+
+#& ".\migrate.exe" TCAndOctopusTest.Db.dll /startUpConfigrationFile=TCAndOctopusTest.Db.dll.config /connectionString=$ConnectionString /connectionProviderName="System.Data.SqlClient" /verbose | Write-Host
